86651b9e2d39d25f392747a2f44550a1aa68af5f
Sean Lee <slee500@users.noreply.github.com>
2016-09-18 18:12:06 -0500

    Update counter.sv

--- counter.sv
+++ counter.sv
@@ -1,12 +1,13 @@
+// We are implementing a 3-bit (0-7) counter
 module counter(
 		input logic enable, clk, reset,
-		output logic [7:0] out);
+		output logic [2:0] out);
 		
 		always_ff @ (posedge clk)
 		begin
 			if (reset) 
 			begin
-				out <= 8'b0;
+				out <= 3'b0;
 			end
 			else if (enable) 
 			begin
